20a9c07a270cfb5b2f2d268fd862c5fa61ed1c27
Alvin Tang <atang@udel.edu>
2016-03-07 19:58:43 -0400

    Answer

--- Triangle/src/main/java/base/Triangle.java
+++ Triangle/src/main/java/base/Triangle.java
@@ -6,6 +6,9 @@ public class Triangle {
  private double Perimeter;
  private double Area;
  private double p;
+ /**
+  * returns a string in the console about the Triangles area and perimeter
+  */
  public String toString() {
 	 return "The area of the triangle is" + this.getArea() +
 			"The perimeter of the triangle is" +this.getPerimeter();
@@ -22,21 +25,39 @@ public double getArea() {
 public double getSideOne() {
 	return sideOne;
 }
+/**
+ * @param sideOne
+ */
 public void setSideOne(double sideOne) {
 	this.sideOne = sideOne;
 }
 public double getSideTwo() {
 	return sideTwo;
 }
+/**
+ * @param sideTwo
+ */
 public void setSideTwo(double sideTwo) {
 	this.sideTwo = sideTwo;
 }
 public double getSideThree() {
 	return sideThree;
 }
+/**
+ * @param sideThree
+ */
 public void setSideThree(double sideThree) {
 	this.sideThree = sideThree;
+/**
+ * Equation for Perimeter of Triangle
+ */
 Perimeter = sideOne + sideTwo + sideThree;
+/**
+ * Perimeter divided by two
+ */
 p= Perimeter/2;
+/**
+ * Area of Triangle
+ */
 Area = Math.sqrt(p*(p-sideOne)*(p-sideTwo)*(p-sideThree));
 }}
